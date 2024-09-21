99d6ba3822e421d1cfebf8abc83273a23d2fa302
Galakeit <galaktia@live.ru>
2024-09-21 16:04:58 +0300

    Secondt_try_JS_HW3
    Update task_2

--- HW3/task_2.js
+++ HW3/task_2.js
@@ -6,5 +6,5 @@ const number_str1 = String(input_number);
 const number_str2 = number_str1 + number_str1;
 const number_str3 = number_str2 + number_str1;
 
-const output_string = number_str1 + number_str2 + number_str3;
+const output_string = +number_str1 + +number_str2 + +number_str3;
 console.log(output_string);
